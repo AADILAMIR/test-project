892ba3c2667fecfb5dcd726971f3efc648464f68
AADILAMIR <theadilamir@gmail.com>
2025-07-28 20:11:54 +0500

    remove extra components

--- src/components/UploadFiles.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <div class="border-dashed border-2 border-gray-300 rounded-md p-6 text-center">
-    <p class="mb-2">Drag files here to upload.</p>
-    <input type="file" multiple class="hidden" />
-    <button class="mt-2 text-sm text-blue-600 underline">Select Files</button>
-  </div>
-</template>
