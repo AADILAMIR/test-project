a9569d766a28edf4373dc01f43d3fd93e33666cb
AADILAMIR <theadilamir@gmail.com>
2025-07-28 19:32:27 +0500

    add dashboard and home view

---
+++ src/views/Dashboard.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <Sections title="Summaries" :items="summaryItems" @cardClick="showToast" />
+    <Sections title="Quick Menu" :items="quickMenuItems" @cardClick="showToast" />
+  </div>
+</template>
+
+<script lang="ts" setup>
+import Sections from '@/components/SectionsComponent.vue'
+import { useToastQueue } from '@/composables/useToastQueue.ts'
+import { ref } from 'vue'
+
+const { show } = useToastQueue()
+const re = ref()
+const summaryItems = [
+  { text: 'Total Records' },
+  { text: 'Records Pending' },
+  { text: 'Processes Pending' },
+  { text: 'Submitted Today' },
+  { text: 'Submitted this Month' },
+]
+
+const quickMenuItems = [
+  { text: 'Add a new record' },
+  { text: 'View past records' },
+  { text: 'Process new records' },
+]
+
+function showToast(item: { text: string }) {
+  show(`You clicked: ${item.text}`, 'success')
+}
+</script>
